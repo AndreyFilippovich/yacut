277eb073424b1aaa3e5c11cc5de2635657a8e1dc
Андрей Филиппович <andrejfilippovic8@gmail.com>
2023-04-21 20:44:44 +0300

    final version v 2.0

--- yacut/views.py
+++ yacut/views.py
@@ -7,31 +7,31 @@ from yacut import app, db
 from yacut.forms import URL_Form
 from yacut.models import URLMap
 
+LETTERS_COUNT = 6
+
 
 def get_unique_short_id():
     letters_digits = string.ascii_letters + string.digits
-    rand_string = ''.join(random.sample(letters_digits, 6))
-    if URLMap.query.filter_by(short=rand_string).first():
-        return get_unique_short_id()
+    rand_string = ''.join(random.sample(letters_digits, LETTERS_COUNT))
+    while URLMap.query.filter_by(short=rand_string).first():
+        rand_string = ''.join(random.sample(letters_digits, LETTERS_COUNT))
     return rand_string
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index_view():
     form = URL_Form()
-    if form.validate_on_submit():
-        short_url = form.custom_id.data
-        if not short_url:
-            short_url = get_unique_short_id()
-        url = URLMap(
-            original=form.original_link.data,
-            short=short_url,
-        )
-        db.session.add(url)
-        db.session.commit()
-        context = {'form': form, 'short_url': short_url}
-        return render_template('main.html', **context)
-    return render_template('main.html', form=form)
+    if not form.validate_on_submit():
+        return render_template('main.html', form=form)
+    short_url = get_unique_short_id() 
+    url = URLMap(
+        original=form.original_link.data,
+        short=short_url,
+    )
+    db.session.add(url)
+    db.session.commit()
+    context = {'form': form, 'short_url': short_url}
+    return render_template('main.html', **context)
 
 
 @app.route('/<string:short_url>')
